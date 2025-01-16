6705a88a1f0664e3022a84d01a5652275311142a
彭亞綺 <62556646+amoee-5272@users.noreply.github.com>
2025-01-16 20:34:15 +0800

    Create hw1_v1.c

---
+++ HW1/hw1_v1.c
@@ -0,0 +1,16 @@
+
+// 20250116 version 1 
+// Can be run on RIPES
+// 2990 cycles
+int T(int n){
+    return (n == 0)? 4 : (4 * T((n - 1) >> 1) + 8 * n + 3);
+    }
+
+
+int main() {
+    int n = 7;
+    int result;
+    result = T(n);
+    printf("answer = %d\n",result);
+    return 0;
+}
