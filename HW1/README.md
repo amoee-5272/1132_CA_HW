33ddb9efb968cc0ee8d0be26b21e508252d9a14c
彭亞綺 <62556646+amoee-5272@users.noreply.github.com>
2025-02-09 03:09:05 +0800

    realign

--- HW1/README.md
+++ HW1/README.md
@@ -11,3 +11,8 @@ https://hackmd.io/@iWmP1H_gSwuc5YgMqmbJFQ/rklreu8DJe/edit
 * hw1_sol_v?.s  可以跑出結果的範例解答
 * hw1.py 用python跑答案
 * hw1_v?.c 用 C code compile 成 assembly 執行 (have been verify on RIPES)
+
+
+
+System id for exit is 10 in Ripes, 93 in GEM5 !
+如果要換平台跑要改
---
+++ HW1/hw1_1.s
@@ -0,0 +1,46 @@
+.data
+input: .word 7
+
+.text
+.global main
+
+# This is 1132 CA Homework 1
+# Implement F(x) = 4*F(floor(n-1)/2) + 8n + 3 , where F(0)=4
+# Input: n in t0(x5)
+# Output: F(n) in a0(x10)
+# DO NOT MOTIFY "main" function
+
+main:        
+	# Load input into t0
+	lw x5, input
+	
+	# Jump to fact   
+	jal t2, fact       
+
+	# Exit program
+    # System id for exit is 10 in Ripes, 93 in GEM5 !
+    li a7, 10
+    ecall
+
+fact:
+	addi sp, sp, -16
+	sw   t2, 8(sp) # store jump address
+	sw   t0, 0(sp) # store n
+	bne  t0, x0, nfact # if n != 0 jump to nfact(recursive core)
+	addi a0, x0, 4 # when n == 0 set return value=4 
+	addi sp, sp, 16
+	jalr t2, t2, 0
+
+nfact:
+	addi t0, t0, -1 # n = n - 1	
+	srli t0, t0, 1 # n = (n - 1) / 2
+	jal t2, fact
+	addi t1, a0, 0 # t1 := fact(n / 2)
+	slli t1, t1, 2 # t1 := fact(n / 2) * 4
+	lw t2, 8(sp)
+	lw t0, 0(sp)
+	addi sp, sp, 16	
+	slli a2, t0, 3 # a2 = 8n
+	add a0, a2, t1 # a0 = 8n + 4f(n / 2) 	
+	addi a0, a0, 3 # a0 = 8n + 4f(n / 2) + 3
+	jalr t2, t2, 0
